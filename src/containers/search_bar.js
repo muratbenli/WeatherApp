23b038c2ce990117a71c0d22e5380ca9fe2d0482
Murat Benli <muratbenli001@gmail.com>
2020-03-30 00:18:19 +0300

    add chart comp. n weather list comp.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -29,13 +29,13 @@ class SearchBar extends Component {
         return (
             <form onSubmit={this.onFormSubmit} className="input-group">
                 <input 
-                    placeholder="Get a five-day forecast in your favorite cities"
+                    placeholder="Hava durumunu merak ettiğiniz şehri giriniz"
                     className="form-control"
                     value={this.state.term}
                     onChange={this.onInputChange}
                 />
                 <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Submit</button>
+                    <button type="submit" className="btn btn-secondary">Ara</button>
                 </span>
             </form>
         );
